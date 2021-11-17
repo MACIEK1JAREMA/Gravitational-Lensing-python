fc634050afad399ced54467db174479b97a2b82f
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2021-11-17 10:35:45 +0000

    Better attempts
    Better attempts were made. It is still very inefficient, and the test result is still not the same as needed, but it getting much closer to a first sucess.

--- attempts/First attempts of grav lensing.py
+++ attempts/First attempts of grav lensing.py
@@ -1,4 +1,4 @@
-# First attetmps at gravitational lensing
+# First attetmps at gravitational lensing, VERY INEFFICIENT, INITIAL VERSION
 
 # Import modules
 import numpy as np
@@ -9,15 +9,26 @@ import matplotlib.pyplot as plt
 # set up some initial parameters
 rc = 0.7
 eps = 0
+dom = 1  # abs() of domain of r values (normally -1, 1 --> 1)
+size = 21  # odd for test
 
 # set up an image of the source. For simplest test, a single pixel at centre
 # As for the initial test, use 21 pixels, empty apart from centre pixel
-size = 21  # odd
-image_s = np.zeros([size, size])
-image_s[int((size-1)/2), int((size-1)/2)] = 1
+image_s = np.zeros([size, size, 3])  # allow for RGB to start with
+image_s[int((size-1)/2), int((size-1)/2), 0] = 1
+
+# plot that image
+plt.imshow(image_s)
 
 # set up an empty array to store lensed image
-image_l = np.zeros([size, size])
+image_l = np.zeros([size, size, 3])
+
+# set up an array of pixel centre coordinates, in reduced
+pixels_l = np.zeros([size, size, 2])
+for i in range(size):
+    for j in range(size):
+        pixels_l[i, j, 0] = 2*dom*i/(size-1) - dom + dom/size
+        pixels_l[i, j, 1] = 2*dom*j/(size-1) - dom + dom/size
 
 
 # loop over tracking back pixels from image_l to image_s via lens eqn
@@ -25,11 +36,45 @@ image_l = np.zeros([size, size])
 for i in range(size):
     for j in range(size):
         # based on pixel position get reduced coords. r_1 and r_2
-        r1 = (1/size)*(2*i - size)
-        r2 = (1/size)*(2*j - size)
+        r1 = 2*dom*i/(size-1) - dom + dom/size
+        r2 = 2*dom*j/(size-1) - dom + dom/size
         # use lens equation to get position on image_s of this pixel
         s1 = r1 - ((1 - eps)*r1)/np.sqrt(rc**2 + (1 - eps)*r1**2 + (1 + eps)*r2**2)
         s2 = r2 - ((1 + eps)*r2)/np.sqrt(rc**2 + (1 - eps)*r1**2 + (1 + eps)*r2**2)
-        # translate s coordinates into pixel coordinates
-        rs1 = size(1+s1)/2
-        rs2 = 
+        # s coordinates dont have to be pixel positions in reduced
+        # need to find which pixel they lie in in the original image
+        # to do so, find the nearest value it maps to in pixel centres corrds.
+        index_1 = (np.abs(pixels_l - s1))[:, 0, 0].argmin()  # abs in order for argmin to funciton correctly
+        index_2 = (np.abs(pixels_l - s2))[0, :, 1].argmin()
+        # check input of these indexes in the original image
+        values = image_s[index_1, index_2, :]
+        # put whatever these are into the lensed image
+        image_l[i, j, :] = values
+
+
+plt.figure()
+# plot the resulting image
+plt.imshow(image_l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
