041208d3769228178ed022417bc7004fdc6fd1b2
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2021-11-23 16:18:59 +0000

    Optimised the lensing via vectorising

--- attempts/stage 2/optimising attempt 1.py
+++ attempts/stage 2/optimising attempt 1.py
@@ -14,7 +14,7 @@ start = timeit.default_timer()
 rc = 0.7
 eps = 0
 dom = 1  # abs() of domain of r values (normally -1, 1 --> 1)
-size = 101  # odd for test
+size = 1024  # odd for test
 
 # set up an image of the source. For simplest test, a single pixel at centre
 # As for the initial test
@@ -30,24 +30,35 @@ image_l = np.zeros([size, size, 3])
 # find the width of each pixel given the domain and size
 p_width = 2*dom/(size)
 
-# loop over tracking back pixels from image_l to image_s via lens eqn
-# Inefficiently for now (initial attempts)
-for i in range(size):
-    for j in range(size):
-        # based on pixel position get reduced coords. r_1 and r_2
-        r1 = 2*dom*i/(size-1) - dom
-        r2 = 2*dom*j/(size-1) - dom
-        # use lens equation to get position on image_s of this pixel
-        s1 = r1 - ((1 - eps)*r1)/np.sqrt(rc**2 + (1 - eps)*r1**2 + (1 + eps)*r2**2)
-        s2 = r2 - ((1 + eps)*r2)/np.sqrt(rc**2 + (1 - eps)*r1**2 + (1 + eps)*r2**2)
-        # s coordinates dont have to be pixel positions in reduced
-        # need to find which pixel they lie in in the original image
-        index_1 = int(np.floor((s1 + dom)/p_width))
-        index_2 = int(np.floor((s2 + dom)/p_width))
-        # check input of these indexes in the original image
-        values = image_s[index_1, index_2, :]
-        # put whatever these are into the lensed image
-        image_l[i, j, :] += values
+# Vectorising the mapping of pixels to source plane and copying data:
+
+# set up an array of index numbers in the lens image array
+i_arr = np.arange(0, size, 1)
+j_arr = np.arange(0, size, 1)
+
+# based on pixel position get reduced coords. r_1 and r_2 as arrays
+r1 = 2*dom*i_arr/(size-1) - dom
+r2 = 2*dom*j_arr/(size-1) - dom
+
+# grid them
+r1g, r2g = np.meshgrid(r1, r2)
+
+# use lens equation to get array of positions on image_s for each pixel in lens image
+s1 = r1g - ((1 - eps)*r1g)/np.sqrt(rc**2 + (1 - eps)*r1g**2 + (1 + eps)*r2g**2)
+s2 = r2g - ((1 + eps)*r2g)/np.sqrt(rc**2 + (1 - eps)*r1g**2 + (1 + eps)*r2g**2)
+
+# s coordinates dont have to be pixel positions in reduced
+# need to find which pixel they lie in in the original image for all pixels
+# therefore for all array values
+index_1 = np.floor((s1 + dom)/p_width)
+index_2 = np.floor((s2 + dom)/p_width)
+
+# change them to integers
+index_1 = index_1.astype(int)
+index_2 = index_2.astype(int)
+
+# copy the data from source image at these indexes over to lens image array
+image_l[:, :, :] = image_s[index_1, index_2, :]
 
 
 plt.figure()
