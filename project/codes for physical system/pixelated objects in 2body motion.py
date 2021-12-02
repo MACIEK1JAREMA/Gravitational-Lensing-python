5dd4ab79ab63c3fbde7514ca1764c2023a65f7e0
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2021-12-02 11:31:50 +0000

    Lensing an eclipsing star-planet pair animation and micorlensing
    Produced an animation of a star and planet eclipsing behind a small, planar, spherically symmetric lensing source. Extracted from it the integrated bolometric luminosity of the pair and produced a microlensing effect.

--- project/codes for physical system/pixelated objects in 2body motion.py
+++ project/codes for physical system/pixelated objects in 2body motion.py
@@ -8,14 +8,28 @@ from PIL import Image
 import scipy
 from scipy import integrate
 import project.lensing_function as lensing
-from matplotlib.lines import Line2D
-import matplotlib.animation as animation
+import matplotlib.widgets as widgets
 
 # %%
 
-# start up an empty source image
+
+# define function to quit animation when button is pressed
+def quit_resp(event):
+    '''
+    Stops the looping animation
+    '''
+    global switchoff
+    # set the exiting loop variable to break
+    switchoff = True
+    # close the window
+    plt.close()
+
+
+# start up an empty source image and lensing parametrers
 size = 200
-image_s = np.zeros((size, size, 3))
+eps = 0
+rc = 0.05
+dom = 2
 
 # #############################################################################
 # simulation of 2 bodies orbiting in plane, for centre positions
@@ -26,7 +40,10 @@ M = 2e30
 m = 2e30
 G = 6.67e-11
 maxR = 1e11  # max planet disp from star, in SI
-dom = 1
+
+# define star and planet sizes in pixels:
+size_s = 10
+size_p = 7
 
 # from size of max orbit get pixel size to scale down
 p_width = maxR/size
@@ -37,9 +54,9 @@ ys = 0
 xp = maxR/2
 yp = 0
 vxs = 0
-vys = 1000
+vys = 20000
 vxp = 0
-vyp = -1000
+vyp = -20000
 
 init_cond = [xs, ys, xp, yp, vxs, vys, vxp, vyp]  # put into a list
 
@@ -49,6 +66,9 @@ t_max = 1 * year
 t_number = 1000
 t_arr = np.linspace(0, t_max, t_number)
 dt = t_arr[-1] - t_arr[-2]
+
+# initialise the list that will store integrated, bolometric 'luminosity'
+lumin_bol = []
 
 
 # set up a fucntion for odeint to use to get needed derivatives
@@ -82,6 +102,9 @@ solution = scipy.integrate.odeint(jacob, init_cond, t_arr)
 xs_anim = solution[:, 0]
 xp_anim = solution[:, 2]
 
+ys_anim = solution[:, 1]
+yp_anim = solution[:, 3]
+
 
 # #############################################################################
 # Animation with pixel placement
@@ -89,54 +112,135 @@ xp_anim = solution[:, 2]
 
 # set up a figure and axis on which to plot the resulting simulation
 fig = plt.figure()
-ax = fig.add_subplot(111, autoscale_on=False, xlim=(-maxR, maxR), ylim=(-maxR, maxR))
+ax = plt.axes([0.1, 0.1, 0.8, 0.8])
+ax.set_xlim(-maxR, maxR)
+ax.set_ylim(-maxR, maxR)
 plt.sca(ax)
 plt.xticks([])  # take off the axis ticks for both, no need for images
 plt.yticks([])
 ax.set_aspect('equal')
 
+# set up axis for timer
+ax_time = plt.axes([0.01, 0.05, 0.18, 0.05])
+ax_time.axes.xaxis.set_visible(False)  # turn off axis visuals for the timer
+ax_time.axes.yaxis.set_visible(False)
+
+# set up a button to stop the aniation
+ax_quit = plt.axes([0.85, 0.85, 0.1, 0.1])
+offHandle = widgets.Button(ax_quit, 'QUIT')
+offHandle.on_clicked(quit_resp)
+
 # set up a timer and draw it on with nothing in it initially
 timer = 'time = %.1f yr'
-time_text = ax.text(0.05, 0.01, '', transform=ax.transAxes)
+ax_time.text(0.05, 0.01, '', transform=ax.transAxes)
 
+# set a stopping variable
+switchoff = False
 
-# set up a function that defines the initial plot for the animation (empty)
-def init():
-    plot = ax.imshow(image_s)
-    time_text.set_text('')
-    return plot, time_text
-
-
-# set up a function to animate uisng FuctAnimate
-def animate(i):
+# loop over the simulation result values and plot as animation, wiht pixels
+for i in range(len(t_arr)):
+    
+    # clear the axis
+    ax.cla()
+    ax_time.cla()
+    
+    # reset the image:
+    image_s = np.zeros((size, size, 3))
+    
     # get the current x values for planet and star
     x = [xs_anim[i], xp_anim[i]]
     
     # scale down to domain and find which pixels these lie in
     # NB y index is always half way up
-    index_s = np.floor((x[0] + dom)/p_width)
-    index_p = np.floor((x[1] + dom)/p_width)
-    index_s = index_s.astype(int) # change them to integers
-    index_p = index_p.astype(int)
+    index_s = np.floor((x[0] + maxR/2)/p_width)
+    index_p = np.floor((x[1] + maxR/2)/p_width) - 1  # zero-indexing
+    index_s = index_s.astype(int).transpose() # change them to integers
+    index_p = index_p.astype(int).transpose()
     
-    # add the central dot to the image:
-    image_s[index_s, int(size/2), :] = 1
-    image_s[index_p, int(size/2), :] = 1
-
-    # replot the image and time text
-    plot = ax.imshow(image_s)
-    time_text.set_text(timer % (i*dt/year), color='white')
+    # set initial checker for if planet is in front:
+    pfront = True
     
-    return plot, time_text
-
-
-# Animate the result
-ani = animation.FuncAnimation(fig, animate, np.arange(1, len(solution)),
-                              interval=25, blit=True, init_func=init)
+    # check which body is in front when in line, using the y data:
+    if abs(index_s + size_s) > abs(index_p - size_p) and abs(index_s - size_s) < abs(index_p + size_p):
+        if yp_anim[i] < ys_anim[i]:
+            pfront = False
+        elif yp_anim[i] > ys_anim[i]:
+            pfront = True
+        else:
+            pass
+    else:
+        pass
+    
+    # draw on the star as a big, yellow circle:
+    for i in range(2*size_s+1):
+        for j in range(2*size_s+1):
+            # get x and y from these relative to star centre
+            x = -size_s + i
+            y = -size_s + j
+            # check if current point is outside the ellipse:
+            if (x/size_s)**2 + (y/size_s)**2 > 1:
+                pass
+            else:
+                # its in ellipse, add its pixel data accordinly
+                try:  # careful about image edges
+                    image_s[int(size/2) - size_s + i, index_s+j, :] += tuple((253, 225, 39))
+                except IndexError:
+                    pass
+    
+    # draw on the planet as a smaller, dark circle:
+    for i in range(2*size_p+1):
+        for j in range(2*size_p+1):
+            # get x and y from these relative to planet centre
+            x = -size_p + i
+            y = -size_p + j
+            # check if current point is outside the ellipse:
+            if (x/size_p)**2 + (y/size_p)**2 > 1:
+                pass
+            else:
+                # its in ellipse, add its pixel data accordinly
+                try:  # careful about image edges
+                    # check if overcasting the sun:
+                    if pfront:
+                        image_s[int(size/2) - size_p + i, index_p+j, :] = tuple((68, 25, 25))
+                    else:
+                        pass
+                except IndexError:
+                    pass
+    
+    
+    # lens the created image:
+    image_lens = lensing.lens(image_s, rc, eps, dom)
+    
+    # update the total luminosty array
+    lumin_bol.append(np.sum(image_lens/255))
+    
+    # plot the lensed image
+    ax.imshow(image_lens/255)
+    
+    # plot the new time too:
+    ax_time.text(0.01, 0.2, 'time = {:.2f} yrs'.format(t_arr[i]/year))
+    
+    # check the quit command:
+    if switchoff:
+        break
+    
+    # pause to update:
+    plt.pause(0.001)
 
 
 # for all time find the CoM position in x and plot to check
 # should not change from 0
 x_cm = (M*xs_anim + m*xp_anim)/(M + m)
+fig_cm = plt.figure()
+ax_cm = fig_cm.gca()
+ax_cm.set_xlabel('time step index')
+ax_cm.set_ylabel(r'$x_{cm}$')
+ax_cm.plot(x_cm)
+
 plt.figure()
-plt.plot(x_cm)
+plt.plot(xs_anim, ys_anim)
+plt.plot(xp_anim, yp_anim)
+
+
+plt.figure()
+plt.plot(t_arr[:len(lumin_bol)], lumin_bol)
